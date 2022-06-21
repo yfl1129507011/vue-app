efef67dbed552373de2bce5dc84948d4dde6ea1e
fenlon <1129507011@qq.com>
2022-06-21 15:42:55 +0800

    完成三级分类的数据动态展示和背景颜色功能

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 
 // 三级联动组件---注册为全局组件
-import TypeNav from '@/pages/Home/TypeNav'
+import TypeNav from '@/components/TypeNav'
 
 Vue.component(TypeNav.name, TypeNav)
 
@@ -12,9 +12,6 @@ import router from '@/router'
 // 引入vuex
 import store from '@/store'
 
-/* import { reqCategoryList } from '@/api'
-
-reqCategoryList() */
 
 Vue.config.productionTip = false
 
--- src/store/home/index.js
+++ src/store/home/index.js
@@ -1,12 +1,24 @@
+import { reqCategoryList } from "@/api"
+
 // vuex模块式编程，home模块数据仓库
 
 const state = { // 仓库存取数据的地方
+    categoryList: []
 }
 
 const mutations = { // 修改state的唯一手段
+    CATGORY_LIST(state, categoryList) {
+        state.categoryList = categoryList
+    }
 }
 
 const actions = { // 可以写自己的业务逻辑，也可以处理异步
+    async categoryList({commit}) {
+        let result = await reqCategoryList()
+        if (result.code == 200) {
+            commit("CATGORY_LIST", result.data)
+        }
+    }
 }
 
 const getters = { // 可以理解为计算属性，用于简化仓库数据，让组件获取仓库的数据更加方便
