bae935c70f426d1e8c63f165f9f882612a0803b6
fenlon <1129507011@qq.com>
2022-06-21 10:16:08 +0800

    Home首页拆分为静态组件

--- src/pages/Home/index.vue
+++ src/pages/Home/index.vue
@@ -1,12 +1,27 @@
 <template>
   <div>
     <TypeNav/>
+    <ListContainer/>
+    <Recommend/>
+    <Rank/>
+    <Like/>
+    <Floor/>
+    <Floor/>
+    <Brand/>
   </div>
 </template>
 
 <script>
+import ListContainer from '@/pages/Home/ListContainer'
+import Recommend from '@/pages/Home/Recommend'
+import Rank from './Rank'
+import Like from './Like'
+import Floor from './Floor'
+import Brand from './Brand'
+
 export default {
-    name: 'Home'
+    name: "Home",
+    components: { ListContainer, Recommend, Rank, Like, Floor, Brand }
 }
 </script>
 
